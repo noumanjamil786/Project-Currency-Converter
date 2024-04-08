4138ffd68767f01599f4beed8cee838eada094d8
Nouman Jamil <159713811+noumanjamil786@users.noreply.github.com>
2024-04-08 17:49:45 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk  from "chalk";
+
+console.log(chalk.green("Welcome To Our Currency Converter Application"));
+const currency : any = {
+    USD : 1,
+    EUR : 0.91,
+    GBP : 0.76,
+    INR : 74.75,
+    PKR : 280,
+}
+let user_answer = await inquirer.prompt([
+    {
+    name : "from",
+    message : "Enter From Currency",
+    type : "list",
+    choices : ["USD","EUR","GBP","INR","PKR"]
+    },
+    {
+        name : "to",
+        message : "Enter To Currency",
+        type : "list",
+        choices : ["USD","EUR","GBP","INR","PKR"]
+    },
+    {
+        name : "Amount",
+        message : "Enter You Amount",
+        type : "number",
+
+    }
+]);
+
+let fromAmount = currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let Amount = user_answer.Amount
+let baseAmount = Amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+console.log(chalk.green.bold(convertedAmount));
+console.log(chalk.red.bold("Thank You For Using My Currency Converter Application"));
+
